d345d5e35b740624852ca4b6fcca72affe936b81
heyu <shy19940918@live.com>
2019-05-20 09:34:08 +0800

    4,5

--- 4.py
+++ 4.py
@@ -7,4 +7,76 @@ def fib(n):  # write Fibonacci series up to n
         print()
 
 
-fib(3000)
+def fib2(n):
+    # Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    else:
+        return result
+
+
+def ask_ok(prompt, retries=4, reminder='please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        elif ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        else:
+            retries = retries - 1
+            if retries < 0:
+                raise ValueError("invalid user response")
+        print(reminder)
+
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+
+def cheeseshop(kind, *name, **dic):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for a in name:
+        print(a)
+    for k in dic:
+        print(k, dic[k])
+
+
+def contact(*args, sep="#"):
+    return sep.join(args)
+
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+
+def lambda_test():
+    pairs = [(1, 'aa', 6), (3, 'ec', 9), (2, 'bc', 8), (4, 'dd', 7)]
+    # 按下标所在对象排序
+    pairs.sort(key=lambda a: a[2])
+    print(pairs)
+
+
+# fib(3000)
+# f = fib
+# print(fib2(500000))
+# ask_ok("Do you really want to quit?")
+# parrot(5, type="gggg")
+# cheeseshop("Limburger", "It's very runny, sir.",
+#            "It's really very, VERY runny, sir.",
+#            shopkeeper="Michael Palin",
+#            client="John Cleese",
+#            sketch="Cheese Shop Sketch")
+# print(contact("a", "c", "b", sep="&"))
+# print(list(range(3, 10)))
+# args = [10, 20]
+# print(list(range(*args)))
+# f = make_incrementor(60)
+# print(f(42))
+# lambda_test()
---
+++ 5.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+num = [1, 4, 3, 3]
+# append
+# num.append(10)
+# print(num)
+
+
+# extend
+# b = [678, 876, 4]
+# b.extend(num.__iter__())
+# print(b)
+
+
+# index
+# print(num.index(3, 0, len(num) - 1))
+
+
+# count
+# print(num.count(3))
+
+# x = ["aaddddddddddddd", "hghb", "cghghgc", "ddg"]
+# sort
+# x.sort(key=lambda s: len(s), reverse=False)
+# print(x)
+
+
+# deque
+# queue = deque(x)
+# print(queue.pop())
+# print(queue.popleft())
+#
+# squares = list(map(lambda d: d ** 2, range(20)))
+# print(squares)
+# squares = [d ** 2 for d in range(20)]
+# print(squares)
+
+# a = [(x, y) for x in range(3) for y in [3, 1, 4] if x != y]
+# a.append(1)
+# print(a)
+
+
