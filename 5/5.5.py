668449833a9c621de3c09481f28703e0278922ee
heyu <shy19940918@live.com>
2019-05-23 16:18:27 +0800

    字典

--- 5/5.5.py
+++ 5/5.5.py
@@ -1,3 +1,21 @@
 # 字典
+tel = {'jack': 14568, 'xxx': 5974}
+print(tel)
 
+print(tel['jack'])
 
+del tel['xxx']
+print(tel)
+
+tel['aaaa'] = 456213
+print(tel)
+
+print(list(tel))
+print(sorted(tel))
+print(tel)
+
+print('aaaa' in tel)
+print('aaaa' not in tel)
+
+a = [tel[x] for x in sorted(tel)]
+print(a)
