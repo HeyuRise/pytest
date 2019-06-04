cb014f30d1d474bea86ff317541733fbd4edbca8
heyu <shy19940918@live.com>
2019-06-04 11:10:38 +0800

    字典

--- 1/2.py
+++ 1/2.py
@@ -10,20 +10,33 @@
 # print(welcome_str.format(**welcome_dic))
 import re
 
-a = input()
-b = input()
-print('a + b = {}'.format(int(a) + int(b)))
+# a = input()
+# b = input()
+# print('a + b = {}'.format(int(a) + int(b)))
 
 
 def parse(text):
-    text = re.sub(r'[^\w]', '', text)
+    text = re.sub(r'[^\w]', ' ', text)
     text = text.lower()
     word_list = text.split(' ')
     word_list = filter(None, word_list)
     word_cnt = {}
     for word in word_list:
-        if word in word_cnt:
+        if word not in word_cnt:
             word_cnt[word] = 0
         word_cnt[word] = word_cnt[word] + 1
+    print(word_cnt)
+    sorted_word_cnt = sorted(word_cnt.items(), key=lambda kv: kv[0], reverse=False)
+    return sorted_word_cnt
+
+
+with open('d:/in.txt', 'r') as fin:
+    text = fin.read()
+
+word_and_freq = parse(text)
+
+with open('d:/out1.txt', 'a') as fout:
+    for word, freq in word_and_freq:
+        fout.write('{}: {}\n'.format(word, freq))
 
 
